2c5d512ad5e22cb3a3f6f98e25a427c16c72b694
Jason Tablas <Jason.Tablas@gompersstudent.org>
2018-03-12 14:28:13 -0700

    Checkpoints
    Finishing up checkpoints

---
+++ src/CheckPoint2.java
@@ -0,0 +1,84 @@
+
+public class CheckPoint2 {
+/*	1. Write a Constructor for a Cat object and set it's name and number of naps. (3) 
+
+
+
+	public class Cat {
+
+       private String name;
+	       private int napsPerDay;
+
+	public Cat(String name, int napsPerDay){
+	 this.name = name;
+	 this.napsPerDay = napsPerDay;
+
+}
+
+	 Cat kitty = new Cat("Kitty", 3);
+
+
+
+
+
+
+
+	}
+
+
+	2. Name three JComponents. (3)
+
+	      Jframe
+
+	     Jpanel
+
+	      Jwindow
+
+	3. Write a method that takes a person’s name as a parameter. If it’s your name, return “awesome”, otherwise return “lame”. (3) 
+
+	
+
+
+
+
+
+
+
+
+
+
+	4. Create variables for the following: (4)
+	      The number of shoes you have
+			int numShoes = 5;
+	      The name of your favorite ice cream
+			String favoriteIceCream = "Vanilla";
+	      Whether you are going to the dance 
+		 String goDance = "no";
+	      A JButton
+		 JButton but = new JButton();
+	      A Cat called Felix
+		Cat felix = new Cat("Felix");
+
+	
+	
+	5. Write code to create strawberry jam and spread it on toast. (7)
+
+
+	public class Jam { 
+
+	      String flavor;
+	       Jam(String flavor) {
+
+	              this.flavor = flavor;
+	       } 
+
+	       void spreadOnToast() {
+
+	              System.out.println("Spreading "+flavor+“ jam on toast”);
+	      }
+	
+	 Jam straw = new Jam(Strawberry);
+	straw.spreadOnToast();
+	}
+*/
+}
---
+++ src/Checkpoint.java
@@ -0,0 +1,51 @@
+
+public class Checkpoint {
+
+//	Fill in the blanks below.
+
+//public class Person {
+
+	//       private int age; 
+
+	 //      private String name;
+
+
+
+//	      public Person ( int age, String name ) {
+
+	//       this.age = age; 
+
+	  //    this.name = name;
+
+	    //  }
+
+
+	      //public String getName () {
+
+//	       return "My name is "+ name;
+//
+	//      }
+
+
+//	      public int getAge () {
+//
+	//       return "I am " + age + " years old";
+
+	    //  }
+
+
+	  //    public void setName ( String name ) {
+
+	      // this.name = name; 
+//
+//	      }
+//
+
+	//      public void setAge (int age ) {
+
+	  //     this.age = age; 
+
+	    //  }
+
+//	}
+}
